7b01246d41c322213c4906aaf15188bc6e656f3a
hieu <nguyentrunghieu17102k2@gmail.com>
2025-03-11 23:28:12 +0700

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import _ from "lodash";
 
 function App() {
+  const arrayData = [
+    "Nguyễn Văn An",
+    "Trần Thị Bích",
+    "Lê Hoàng Nam",
+    "Phạm Hồng Phúc",
+    "Đặng Thị Mai",
+    "Bùi Minh Quân",
+    "Hoàng Ngọc Anh",
+    "Võ Thanh Tú",
+    "Đỗ Hải Yến",
+    "Ngô Văn Dũng",
+  ];
+
+  const surnameSort = _.orderBy(arrayData, [(name) => name], ["desc"]);
+
+  const lastNameSort = _.orderBy(
+    arrayData,
+    [(name) => name.split(" ").pop()],
+    ["asc"]
+  );
+
+  const groupedData = _.groupBy(lastNameSort, (name) =>
+    name.split(" ").pop()[0].toUpperCase()
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        {surnameSort.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </div>
+      <hr />
+      <div>
+        {lastNameSort.map((item, index) => (
+          <div key={index}>{item}</div>
+        ))}
+      </div>
+      <hr />
+      <div>
+        {Object.entries(groupedData).map(([letter, names]) => (
+          <div key={letter}>
+            <h3>{letter}:</h3>
+            {names.map((name, index) => (
+              <div key={index}>{name}</div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
